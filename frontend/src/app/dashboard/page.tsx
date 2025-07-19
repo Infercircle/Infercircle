c902bc5a91b3f97272f634a544a0358f06adff6e
ifechukwuobiezue <ifechukwuobiezuedoc@gmail.com>
2025-07-19 21:12:58 +0100

    fix

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Dashboard from "@/components/overview/Dashboard";
 
 export default function OverviewPage() {
